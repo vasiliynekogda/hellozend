50f02a7f054f7c04cd3d2e7fc9a126dca6685e09
Vasiliy Nekogda <vnekogda@speroteck.com>
2014-02-24 12:23:16 +0200

    standard update

--- application/controllers/IndexController.php
+++ application/controllers/IndexController.php
@@ -5,17 +5,12 @@ class IndexController extends Zend_Controller_Action
 
     public function init()
     {
-        // $valid = new Formvalidator('Type');
-        //$this->view->valid = $valid->valid;
+        
     }
     public function indexAction()
     {
 
-
-
-
-
-        if(isset($_POST['name'])) {
+        if (isset($_POST['name'])) {
             $show = '<p>Hello, ' .$_POST['name']. '</p>';
         } else {
             $show = new Application_Form_Login();
